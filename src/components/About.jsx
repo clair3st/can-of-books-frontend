0164177dab28a1697246f86dd45f302293634d7d
clair3st <clairejgatenby@gmail.com>
2024-10-25 11:07:58 -0700

    updated about me

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Profile extends Component {
+
+  render() {
+    
+    return <p>I'm Claire and I developed this while working through the codefellows 301 react course. Thanks for visiting my App!</p>
+  }
+}
+
+export default Profile;
