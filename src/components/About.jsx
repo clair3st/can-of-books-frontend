efd3f01432ececbcdd6965b96677ee1260acb51c
clair3st <clairejgatenby@gmail.com>
2024-10-25 11:20:56 -0700

    added link to gh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ class Profile extends Component {
 
   render() {
     
-    return <p>I'm Claire and I developed this while working through the codefellows 301 react course. Thanks for visiting my App!</p>
+    return <p>I'm <a href="https://github.com/clair3st">Claire</a> and I developed this while working through the codefellows 301 react course. Thanks for visiting my App!</p>
   }
 }
 
