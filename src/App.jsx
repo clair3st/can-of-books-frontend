0e4568d6ab55839f885fadd677362e68baba6300
clair3st <clairejgatenby@gmail.com>
2024-10-25 11:18:33 -0700

    added route to about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import BestBooks from './components/BestBooks';
+import About from './components/About'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -23,7 +24,11 @@ class App extends React.Component {
               element={<BestBooks />}
             >
             </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route 
+              exact path="/about"
+              element={<About />}
+            >
+            </Route>
           </Routes>
           <Footer />
         </Router>
