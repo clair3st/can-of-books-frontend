d48eb6e2f8763e21ed3026b48ec547eaf34a3e81
clair3st <clairejgatenby@gmail.com>
2024-10-25 11:04:30 -0700

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css';
+
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import BestBooks from './components/BestBooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
