48ed12fe5f7399be39aac371fa1d05a74e356d09
clair3st <clairejgatenby@gmail.com>
2024-10-26 15:23:23 -0700

    updated app structure to pass state to children

--- src/components/BestBooks.jsx
+++ src/components/BestBooks.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import axios from 'axios';
-
-import Carousel from 'react-bootstrap/Carousel';
+import AddBook from './AddBook.jsx'
+import ErrorMessage from './ErrorMessage'
+import { Carousel } from 'react-bootstrap';
+const API_URI = import.meta.env.VITE_IS_DEV == 'true' ? import.meta.env.VITE_BOOKS_API_DEV : import.meta.env.VITE_BOOKS_API_PROD;
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      active: false,
+      new_book: {
+        title: '',
+        description: '',
+        status: ''
+      },
+      error_message: ''
     }
 
   }
 
   async componentDidMount() {
-    const API_URI = import.meta.env.VITE_IS_DEV == 'true' ? import.meta.env.VITE_BOOKS_API_DEV : import.meta.env.VITE_BOOKS_API_PROD;
     console.log(API_URI)
     axios.get(`${API_URI}/books`)
       .then(response =>{
@@ -25,25 +33,68 @@ class BestBooks extends React.Component {
       })
   }
 
+  handleModalOpen = () => {
+      this.setState({active:true})
+  }
+  handleModalClose = () =>{
+    this.setState({active:false})
+  }
+
+  handleNewBookForm = (e) => {
+    const newBookProps = {
+        ...this.state.new_book,
+        [e.target.name]:  e.target.value
+    }
+    this.setState({new_book: newBookProps});
+  }
+
+  handleNewBookSubmit = async (e) => {
+    e.preventDefault();
+    this.handleModalClose();
+
+    await axios.post(`${API_URI}/books`, null, {params: this.state.new_book})
+    .then((response => {
+      console.log('successful post', response)
+        if('title' in response.data){
+          this.setState({books: [...this.state.books, response.data]})
+        }
+      }).bind(this))
+      .catch( (error => {
+        let msg = 'response' in error ? error.response.data : error.message
+        this.setState({error_message:msg})
+      }).bind(this));
+  }
+
   render() {
 
     console.log('render', this.state)
     return (
       <>
-        <h2 className="m-4">My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        <div className="d-flex justify-content-between align-items-center m-4">
+          <h2 >My Essential Lifelong Learning &amp; Formation Shelf</h2>
+          <div>
+            <AddBook active={this.state.active}
+                     handleOpen={this.handleModalOpen}
+                     handleClose={this.handleModalClose}
+                     handleSubmit={this.handleNewBookSubmit}
+                     handleNewBook={this.handleNewBookForm}
+                     />
+          </div>
+        </div>
+        <div>
+          <ErrorMessage message={this.state.error_message} />
+        </div>
         { 
           this.state.books.length ? (
           <Carousel data-bs-theme="dark">
           {this.state.books.map((book,i) =>{
             return (
               <Carousel.Item key={i}>
-              
-                
+                   
                 <h3>{book.title}</h3>
                 <p>{book.description}.</p>
               
-            </Carousel.Item>
+              </Carousel.Item>
             )
 
           })}
